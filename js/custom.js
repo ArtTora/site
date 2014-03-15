82edff9272e635e3c86c32a101f229a2964568f3
alexes.dev <alexes.dev@gmail.com>
2014-03-15 20:33:35 +0200

    remove preloader

--- js/custom.js
+++ js/custom.js
@@ -136,13 +136,4 @@ $(document).ready(function() {
 
 
   });
-  //preloader  
-
-  // makes sure the whole site is loaded
-  jQuery(window).load(function() {
-    // will first fade out the loading animation
-    jQuery("#status").fadeOut();
-    // will fade out the whole DIV that covers the website.
-    jQuery("#preloader").delay(1000).fadeOut("slow");
-  })
 });
