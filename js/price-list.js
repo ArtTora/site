3d0b846e654a17d0a63f4bceb190740b83a5a72d
Alexes <alexes.dev@gmail.com>
2014-03-27 16:18:31 +0000

    fix price list

--- js/price-list.js
+++ js/price-list.js
@@ -1,137 +1,70 @@
-ko.bindingHandlers.fancySelect = {
-    init: function(element, valueAccessor, allBindingsAccessor){
-        var obj = valueAccessor();
-
-        setTimeout(function(){
-            $(element).fancySelect({}).on('change.fs', function(){
-                var binds = allBindingsAccessor();
-                var options = allBindingsAccessor().options();
-                var value = jQuery(this).val();
-
-                if(options){
-                    for(var i = 0; i < options.length; ++i){
-                        if(options[i][binds.optionsValue] == value){
-                            obj(options[i]);
-                            $('select').trigger('update.fs');
-                        }
-                    }
-                }
-            });
-        }, 1);
-    }
-};
-
 (function(global){
-    var PriceList = function(data){
-        var self = this;
+    var allDate = {};
+    var itemNames = ['formats', 'chromaticities', 'paper_types'];
 
-        self.items = ko.observableArray(data);
-        self.formats = ko.observableArray([]);
-        self.copyCounts = ko.observableArray([]);
-        self.chromaticities = ko.observableArray([]);
-        self.paperTypes = ko.observableArray([]);
-        self.options = ko.observableArray([]);
+    var getItems = function(item){
+        var items = item.formats || item.chromaticities || item.paper_types;
 
-        self.selectedItem = ko.observable();
-        self.selectedItem.subscribe(function(value){
-            if(value)
-                self.formats(value.formats);
-            else {
-                self.selectedFormat(null);
-                self.selectedChromaticite(null);
-                self.selectedPaperType(null);
-                self.selectedCopyCount(null);
+        if(!items && item.copyCount){
+            items = [];
+
+            for(var i = 0; i < item.copyCount.length; ++i){
+                items.push({ name: item.copyCount[i][0] });
             }
-        });
+        }
 
-        self.selectedFormat = ko.observable();
-        self.selectedFormat.subscribe(function(value){
-            if(value)
-                self.chromaticities(value.chromaticities);
-            else {
-                self.selectedChromaticite(null);
-                self.selectedPaperType(null);
-                self.selectedCopyCount(null);
-            }
-        });
-
-        self.selectedChromaticite = ko.observable();
-        self.selectedChromaticite.subscribe(function(value){
-            if(value)
-                self.paperTypes(value.paper_types);
-            else {
-                self.selectedPaperType(null);
-                self.selectedCopyCount(null);
-            }
-        });
-
-        self.selectedPaperType = ko.observable();
-        self.selectedPaperType.subscribe(function(value){
-            if(value && value.copyCount){
-                var data = [];
-
-                for(var i = 0; i < value.copyCount.length; ++i){
-                    data.push({ value: value.copyCount[i][0], price: value.copyCount[i][1] });
-                }
-
-                self.copyCounts(data);  
-            } else {
-                self.selectedCopyCount(null);
-            }
-        });
-
-        self.selectedCopyCount = ko.observable();
-        self.selectedCopyCount.subscribe(function(value){
-            var allOptions = self.selectedPaperType() ? self.selectedPaperType().options : null;
-
-            if(value && allOptions){
-                var options = [];
-
-                for(var i = 0; i < allOptions.length; ++i){
-                    var option = allOptions[i];
-
-                    for(var ii = 0; ii < option.prices.length; ++ii){
-                        var price = option.prices[ii];
-
-                        if(price[0] == value.value){
-                            options.push(option);
-                        }
-                    }
-                }
-
-                self.options(options);
-            }
-        });
-
-        self.checkedOptions = ko.observableArray([]);
-
-        self.totalSum = ko.computed(function(){
-            var copyCount = self.selectedCopyCount();
-            var options = self.checkedOptions();
-
-            if(copyCount){
-                var total = copyCount.price;
-
-                for(var i = 0; i < options.length; i++){
-                    var option = options[i];
-
-                    for(var ii = 0; ii < option.prices.length; ++ii){
-                        var price = option.prices[ii];
-
-                        if(price[0] == copyCount.value){
-                            total += price[1];
-                        }
-                    }
-                }
-
-                return total;
-            }
-
-            return 0;
-        });
+        return items;
     };
 
-    global.bindPriceList = function(data){
-        ko.applyBindings(new PriceList(data));
+    var buildSelect = function(list){
+        if(!jQuery.isArray(list)){
+            console.warn('Unexpected type');
+            return;
+        }
+
+        var items = '<select><option></option>';
+
+        jQuery.each(list, function(index, item){
+            items += '<option value="' + index + '">' + item.name + '</option>';
+        });
+
+        return jQuery(items + '</select>');
+    };
+
+    var step = function(list, parentContainter){
+        var container = jQuery('<div />', { class: 'step' }).appendTo(parentContainter);
+        var selectContainer = jQuery('<div />', { class: 'input' }).appendTo(container);
+        var nextStepContainer = jQuery('<div />', { class: 'next-step' }).appendTo(container);
+        var select = buildSelect(list);
+        var nextStep;
+
+        if(select){
+            selectContainer.append(select);
+            select.change(function(){
+                nextStepContainer.empty();
+
+                if(!select.val())
+                    return;
+
+                var items = getItems(list[select.val()]);
+
+                if(items){
+                    nextStep = step(items, nextStepContainer);
+                } else {
+                    console.log('result');
+                }
+            });
+        }
+    };
+
+    jQuery(function(){
+        jQuery.each(allDate, function(key, list){
+            var container = jQuery('.' + key + '-container');
+            var firstStep = step(list, container);
+        });
+    });
+
+    global.bindPriceList = function(type, data){
+        allDate[type] = data;
     };
 })(window);
