71c8ea2f72367e59f7119f0edffcd5e903a884bb
Alexes <alexes.dev@gmail.com>
2014-03-27 16:45:59 +0000

    add options

--- js/price-list.js
+++ js/price-list.js
@@ -16,6 +16,24 @@
         return items;
     };
 
+    var getOptions = function(value, options){
+        var result = [];
+
+        for(var i = 0; i < options.length; ++i){
+            var option = options[i];
+
+            for(var ii = 0; ii < option.prices.length; ++ii){
+                var price = option.prices[ii];
+
+                if(price[0] == value){
+                    result.push(option);
+                }
+            }
+        }
+
+        return options;
+    };
+
     var buildSelect = function(list){
         if(!jQuery.isArray(list)){
             console.warn('Unexpected type');
@@ -31,27 +49,45 @@
         return jQuery(items + '</select>');
     };
 
+    var options = function(list, parentContainter){
+        var items = '';
+
+        jQuery.each(list, function(index, item){
+            items += '<input type="checkbox" /> ' + item.name;
+        });
+
+        parentContainter.append(items);
+    };
+
     var step = function(list, parentContainter){
         var container = jQuery('<div />', { class: 'step' }).appendTo(parentContainter);
         var selectContainer = jQuery('<div />', { class: 'input' }).appendTo(container);
         var nextStepContainer = jQuery('<div />', { class: 'next-step' }).appendTo(container);
+        var currentOptionsContainer = jQuery('<div />', { class: 'current-options' }).appendTo(container);
         var select = buildSelect(list);
-        var nextStep;
+        var nextStep, currentOptions;
 
         if(select){
             selectContainer.append(select);
             select.change(function(){
                 nextStepContainer.empty();
+                currentOptionsContainer.empty();
 
                 if(!select.val())
                     return;
 
-                var items = getItems(list[select.val()]);
+                var currentItem = list[select.val()];
+                var items = getItems(currentItem);
 
                 if(items){
+                    items.parent = currentItem;
                     nextStep = step(items, nextStepContainer);
                 } else {
                     console.log('result');
+                }
+
+                if(list.parent && list.parent.options){
+                    currentOptions = options(getOptions(currentItem.name, list.parent.options), currentOptionsContainer);
                 }
             });
         }
